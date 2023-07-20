f1047407191b6b0be828b7c4fd338d2932f56329
jiaqchen <jiaqchen@ethz.ch>
2023-07-20 16:15:40 +0200

    client server updates

--- client/scripts.js
+++ client/scripts.js
@@ -1,15 +1,38 @@
+// var fs = require("fs");
+// module.exports = {
+// 	cert: fs.readFileSync(__dirname + "../host.crt"),
+// 	key: fs.readFileSync(__dirname + "../host.key"),
+// 	passphrase: "12345"
+// };
+
 function getNextImage() {
-    // Get button and remove it if it exists
-    if (document.getElementById("next-image") != null) {
-        document.getElementById("next-image").remove();
-    }
+    // // Get button and remove it if it exists
+    // if (document.getElementById("next-image") != null) {
+    //     document.getElementById("next-image").remove();
+    // }
 
-    // Get another image from local folder for labeling-image
-    var image = document.getElementById("labeling-image");
+    // Change the name of "next-image" button to "Skip this GIF"
+    var button = document.getElementById("next-image");
+    button.innerHTML = "Skip this GIF";
 
-    // 
-    image.src = "/home/julia/Documents/h_coarse_loc/data/3DSSG/3RScan/" + Math.floor(Math.random() * 10) + ".gif";
-
+    // Fetch a new image from the server using get_image
+    fetch('https://129.132.245.9:5000/getimage', {
+        method: 'GET'
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        // Create a local URL for the image
+        var imageUrl = URL.createObjectURL(blob);
+        var image = document.getElementById("labeling-image");
+        // Rotate the image 90 degrees clockwise before displaying
+        image.style.transform = "rotate(90deg)";
+        // Make gif fit in the div
+        image.style.maxWidth = "100%";
+        image.src = imageUrl;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
 function sendData() {
@@ -25,7 +48,7 @@ function sendData() {
 
     console.log(data)
 
-    fetch('http://127.0.0.1:5000/annotateme', {
+    fetch('https://129.132.245.9:5000/handledata', {
         method: 'POST', 
         headers: {
             'Content-Type': 'application/json',
@@ -44,8 +67,29 @@ function sendData() {
     document.getElementById("annotation").value = "";
 
     // Change button contents randomly based on a list of options
-    var options = ["Done! Next GIF please!", "That was easy! Next GIF please!", "I'm on a roll! Next"];
-
+    var options = ["Done! Next GIF please!", 
+                   "That was easy! Next GIF please!", 
+                   "I'm on a roll! Next", 
+                   "Submit! You're making it too easy for me.",
+                   "I'm a pro! Next GIF please!",
+                   "Again! Next GIF please!",
+                   "Next GIF please!",
+                   "Give me a harder GIF!",
+                   "Next GIF please!",
+                   "Submit! MORE!",
+                   "Submit! Next GIF please!",
+                   "I'm going to be done in no time! Next GIF please!",
+                   "I'm a labeling machine! Next GIF please!",
+                   "I'm a labeling god! Next GIF please!",
+                   "I can't get enonugh! Next GIF please!",
+                   "You're making it too easy for me! Next GIF please!",
+                   "I'm the captioning master! Next GIF please!",
+                   "I'm the lord of labeling! Next GIF please!",
+                   "That was fun! Next GIF please!",
+                   "MOREEEEEEEE!"];
+    var button = document.getElementById("submit-button");
+    button.innerHTML = options[Math.floor(Math.random() * options.length)];
+ 
     // Get the next image
     getNextImage();
 }
--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 import json
+import os
+import random
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/annotateme', methods=['POST'])
+@app.route('/handledata', methods=['POST'])
 def handle_data():
     # Get the text from the POST request. with headers and body
     text = request.get_json()
@@ -14,13 +16,40 @@ def handle_data():
     print(text)
 
     # write text to a data json file
-    with open('./data.json', 'a') as outfile:
-        json.dump(text, outfile)
-        outfile.write('\n')
+    try:
+        with open('./data.json', 'a') as outfile:
+            print("Writing to data.json")
+            json.dump(text, outfile)
+            outfile.write('\n')
+    except:
+        print("Error writing to data.json")
 
     return('Success!')
 
+@app.route('/getimage', methods=['GET'])
+def get_image():
+    print("Getting image")
+    # Get image from /data
+    scans = os.listdir(app.static_folder)
+
+    # Randomly select a scan
+    scan = random.choice(scans)
+
+    # Access GIFs inside scan
+    gifs = os.listdir(os.path.join(app.static_folder, scan))
+
+    # Randomly select a GIF
+    gif = random.choice(gifs)
+
+    print(gif)
+
+    # Return the gif to client POST request
+    return send_from_directory(os.path.join(app.static_folder, scan), gif)
+
 if __name__ == '__main__':
     # Run server, print terminal
-    app.run()
+    # app.run(host='129.132.245.9', port=5000)
+    # app.run()
+	# app.run(ssl_context=('eduCBAcert.pem', 'eduCBAkey.pem'))
+    app.run(ssl_context='adhoc')
 
